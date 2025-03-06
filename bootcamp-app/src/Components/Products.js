8ca5490bfd7db1b3a71563f69368ef503c9c137a
shobhit <shobhitgaidhane105@gmail.com>
2025-03-06 18:04:50 +0530

    commit

--- bootcamp-app/src/Components/Products.js
+++ bootcamp-app/src/Components/Products.js
@@ -1,9 +1,34 @@
 import React from 'react'
-
+const products = [
+    { name: "Mobile", price: 200 },
+    { name: "Laptop", price: 800 },
+    { name: "Tablet", price: 300 },
+    { name: "Smartwatch", price: 150 },
+    { name: "Headphones", price: 100 },
+    { name: "Monitor", price: 400 },
+    { name: "Keyboard", price: 50 },
+    { name: "Mouse", price: 30 },
+    { name: "Speaker", price: 250 },
+    { name: "Camera", price: 600 }
+  ];
+  
+  console.log(products);
+  
 export default function Products() {
   return (
-    <div>
-    <h1>All Products</h1>
+    <div className='row'>
+    {
+        products.map(products => {
+            return(
+                <div className='col-3'>
+                    hello
+                </div>
+
+            )
+        })
+    }
+    
     </div>
+    
   )
 }
